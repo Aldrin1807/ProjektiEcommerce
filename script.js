13f7a4f6333854672f2e8dc37041887feee19cd2
Aldrin1807 <ai58914@ubt-uni.net>
2022-12-05 21:14:42 +0100

    Added produkti file and fixed some css

---
+++ script.js
@@ -0,0 +1,20 @@
+// function validate(){
+//     firstN = document.getElementById("fname").value;
+//     lastN = document.getElementById("lname").value;
+//     userName = document.getElementById("uName").value;
+//     Email = document.getElementById("email").value;
+//     passW = document.getElementById("pass").value;
+//     if(firstN==''||lastN==''||userName==''||Email==''||passW==''){
+//         document.getElementById("fname").style.borderColor = "red";
+//         document.getElementById("lname").style.borderColor = "red";
+//         document.getElementById("uName").style.borderColor = "red";
+//         document.getElementById("email").style.borderColor = "red";
+//         document.getElementById("pass").style.borderColor = "red";
+//     }else{
+//         document.getElementById("fname").style.borderColor = "black";
+//         document.getElementById("lname").style.borderColor = "black";
+//         document.getElementById("uName").style.borderColor = "black";
+//         document.getElementById("email").style.borderColor = "black";
+//         document.getElementById("pass").style.borderColor = "black";
+//     }
+// }
