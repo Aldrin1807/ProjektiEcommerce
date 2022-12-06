065754bc4722bcd3fb4b10c7f1496f0f6b35bf36
Aldrin1807 <ai58914@ubt-uni.net>
2022-12-06 19:49:33 +0100

    changes

--- script.js
+++ script.js
@@ -18,3 +18,44 @@
 //         document.getElementById("pass").style.borderColor = "black";
 //     }
 // }
+// const slides = document.querySelectorAll(".slide");
+// var counter=0;
+// // console.log(slides);
+// slides.forEach(
+//     (slide,index) =>{
+//         slide.style.left = `${index*100}%`
+//     }
+// )
+// const goPrev = () =>{
+//     counter--
+//     slideImage();
+// }
+// const goNext = () =>{
+//     counter++
+//     slideImage();
+// }
+
+// const slideImage = () => {
+//     slides.forEach(
+//         (slide)=>{
+//             slide.style.transform = `translateX(-${counter*100}%)`
+//         }
+//     )
+// }
+// var i=0;
+// var images = []
+// var time =3000;
+
+// images[0] = ";
+// images[1] = "";
+
+// function changeImg(){
+//     document.slide.src = images[i];
+//     if(i < images.length -1){
+//         i++;
+//     }else{
+//         i=0;
+//     }
+//     setTimeout("changeImg()",time);
+// }
+// window.onload = changeImg;
