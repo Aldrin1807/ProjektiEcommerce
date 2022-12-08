a7b1f256f8e97adbc6748b9b31916884700e30ea
Aldrin1807 <ai58914@ubt-uni.net>
2022-12-09 00:02:00 +0100

    small changes

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,12 +1,13 @@
-const emri = document.getElementById('nname');
-const emailii = document.getElementById('nemail');
-const number = document.getElementById('nnumber');
-const subject = document.getElementById('subject');
+const emri = document.getElementById('name');
+const emailii = document.getElementById('email');
+
+const subject = document.getElementById('c1');
 
 const emailRegex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-function login(){
+function send(){
+    if(emri.value.trim()==''||emri.value.length<=2||emailii.value.trim()==''||!emailRegex.test(emailii.value)||subject.value.trim()==''||subject.value.length<=10){
     if(emri.value.trim()==''||emri.value.length<=2){
         emri.style.border='2px solid red';
     }else{
@@ -17,15 +18,12 @@ function login(){
     }else{
         emailii.style.border='2px solid green';
     }
-    if(number.value.trim()==''||number.value.length<=8){
-        number.style.border='2px solid red';
-    }else{
-        number.style.border='2px solid green';
-    }
     if(subject.value.trim()==''||subject.value.length<=10){
         subject.style.border='2px solid red';
     }else{
         subject.style.border='2px solid green';
     }
-    
-}
+    }else{
+        window.location.replace('contact.html');
+    }
+};
