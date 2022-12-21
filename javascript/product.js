03e9438ea378fbc6e9c15391d44348ae81577f62
Aldrin1807 <ai58914@ubt-uni.net>
2022-12-21 18:12:13 +0100

    changes

--- javascript/product.js
+++ javascript/product.js
@@ -15,70 +15,72 @@ var paragrafi = document.getElementById('paragrafi');
 var imgs = document.getElementById('fotoja');
 
 //Fotot per secilin produkt
-var img1= '../images/1.jpg';
-var img2= '../images/2.jpg';
-var img3= '../images/3.jpg';
-var img4= '../images/4.jpg';
-var img5= '../images/5.jpg';
-var img6= '../images/6.jpg';
-var img7= '../images/7.jpg';
-var img8= '../images/8.jpg';
+var images = ['../images/1.jpg','../images/2.jpg','../images/3.jpg','../images/4.jpg','../images/5.jpg','../images/6.jpg','../images/7.jpg','../images/8.jpg'];
+// var img1= '../images/1.jpg';
+// var img2= '../images/2.jpg';
+// var img3= '../images/3.jpg';
+// var img4= '../images/4.jpg';
+// var img5= '../images/5.jpg';
+// var img6= '../images/6.jpg';
+// var img7= '../images/7.jpg';
+// var img8= '../images/8.jpg';
 
 
 //Funksioni qe i ndryshon elementet varesisht prej parametrit
 function produktiNR(n){
     if(n==1){
-        imgs.src=img1;
-        h1shi.innerHTML= 'Produkti 1';
-        h3shi.innerHTML='500 Euro';
-        paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';  
+        imgs.src=images[0];
+        h1shi.innerHTML= 'Eco-Friendly Bamboo Chair';
+        h3shi.innerHTML='80 $';
+        paragrafi.innerHTML = 'The "Eco-Friendly Bamboo Chair" is a stylish and sustainable seating option made from 100% natural bamboo. It features a comfortable padded seat and backrest, and its lightweight design makes it easy to move around. ';  
         }else if(n==2){
-            imgs.src=img2;
-            h1shi.innerHTML= 'Produkti 2';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[1];
+            h1shi.innerHTML= 'The "Luxury Memory Foam Mattress';
+            h3shi.innerHTML='2000 $';
+            paragrafi.innerHTML = 'The "Luxury Memory Foam Mattress" is a high-quality and comfortable bed that conforms to your bodys shape for ultimate support. It has a thick layer of memory foam and a supportive base layer, and its hypoallergenic design makes it perfect for allergy sufferers. ';
         }else if(n==3){
-            imgs.src=img3;
-            h1shi.innerHTML= 'Produkti 3';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[2];
+            h1shi.innerHTML= 'Contemporary Platform Bed';
+            h3shi.innerHTML='1500 $';
+            paragrafi.innerHTML = 'The "Contemporary Platform Bed" is a modern and sleek bed that is perfect for a minimalistic bedroom. It has a low profile design with a sturdy wooden frame and a slatted base, and its simple design makes it easy to pair with any decor. ';
         }else if(n==4){
-            imgs.src=img4;
-            h1shi.innerHTML= 'Produkti 4';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[3];
+            h1shi.innerHTML= 'Rustic Farmhouse Bed';
+            h3shi.innerHTML='1350 $';
+            paragrafi.innerHTML = 'The "Rustic Farmhouse Bed" is a stylish and classic bed that adds a touch of charm to any bedroom. It has a sturdy wooden frame and a distressed finish, and its simple design makes it a versatile and timeless piece. ';
         }else if(n==5){
-            imgs.src=img5;
-            h1shi.innerHTML= 'Produkti 5';
-            h3shi.innerHTML='490 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[4];
+            h1shi.innerHTML= 'Orthopedic Memory Foam Mattress';
+            h3shi.innerHTML='1450 $';
+            paragrafi.innerHTML = 'The "Orthopedic Memory Foam Mattress" is a high-quality and supportive bed that is perfect for people with back or joint pain. It has a thick layer of memory foam and a supportive base layer, and its orthopedic design helps to align the spine and reduce pressure points. ';
         }else if(n==6){
-            imgs.src=img6;
-            h1shi.innerHTML= 'Produkti 6';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[5];
+            h1shi.innerHTML= 'Hybrid Innerspring Mattress';
+            h3shi.innerHTML='1150 $';
+            paragrafi.innerHTML = 'The "Hybrid Innerspring Mattress" is a comfortable and supportive bed that combines the benefits of memory foam and innerspring coils. It has a layer of memory foam for pressure relief and a layer of coils for support, and its medium-firm feel makes it suitable for most sleepers. ';
         }else if(n==7){
-            imgs.src=img7;
-            h1shi.innerHTML= 'Produkti 7';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[6];
+            h1shi.innerHTML= 'Wingback Armchair';
+            h3shi.innerHTML='50 $';
+            paragrafi.innerHTML = 'The "Wingback Armchair" is a classic and stylish chair that adds a touch of elegance to any room. It has a high backrest with wings and padded armrests, and its upholstered seat and backrest provide comfort and support. ';
         }else if(n==8){
-            imgs.src=img8;
-            h1shi.innerHTML= 'Produkti 8';
-            h3shi.innerHTML='500 Euro';
-            paragrafi.innerHTML = 'Ky produkt eshte bla bla bla ';
+            imgs.src=images[7];
+            h1shi.innerHTML= 'Compact Armchair';
+            h3shi.innerHTML='50 $';
+            paragrafi.innerHTML = 'The "Compact Armchair" is a stylish and space-saving chair that is perfect for small living spaces. It has a sleek design with a narrow profile and a low backrest, and its padded seat and backrest provide comfort and support. Its compact size makes it easy to fit in tight spaces, and its lightweight design makes it easy to move around.';
         }
     };
 
-
+//klasa per variablen statike
     class VStatic{
         static n = 1;
         static updateStatic(newValue){
             this.n=newValue;
+            //shtimi i variables statike ne local storage pastaj per tu aksesuar ne faqen tjeter
             localStorage.setItem("myStaticVariable", newValue);
         }
     }
-
+//funksioni onclick i cili e ndrron faqen dhe ja ndryshon vleren variables statike
     function redirectTo(num){
         console.log('clicked');
         VStatic.updateStatic(num);
@@ -87,6 +89,7 @@ function produktiNR(n){
         
     }
     console.log(VStatic.n);
+    //thirrja e funksionit per ndryshimin e elementeve duke aksesuar variablen statike
     function change(){
         VStatic.n = localStorage.getItem("myStaticVariable");
         produktiNR(VStatic.n);
--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,14 @@
-// const emaili = document.getElementById('Lemail');
-// const passwordi = document.getElementById('Lpassword');
-// const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-// function login(){
-// if(emaili.value.trim()==''||!emailRegex.test(emaili.value)){
-//   emaili.style.border='2px solid red';
-// }else{
-//   emaili.style.border='1px solid black';
-// }
-// if(passwordi.value.trim()==''||passwordi.value.length<8){
-//   passwordi.style.border='2px solid red';
-// }else{
-//   passwordi.style.border='1px solid black';
-// }
-// }
+var vargu = ['/images/bg.webp','/images/bg1.webp','/images/bg2.webp'];
+
+var i=0;
+const koha = 2500;
+elementi = document.getElementById('mbajtesi');
+elementi.style.transition = 'background 0.5s ease-in-out';
+function krijoSlider(){
+    elementi.style.backgroundImage = `url(${vargu[i]})`;
+    i++;
+    if(i==vargu.length){
+        i=0;
+    }
+    setTimeout('krijoSlider()',koha);
+};
