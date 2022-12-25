fc7569283b329bfd2960f2e36cc00681b2791545
Aldrin1807 <ai58914@ubt-uni.net>
2022-12-25 13:09:17 +0100

    fixed password regex

--- javascript/register.js
+++ javascript/register.js
@@ -7,7 +7,7 @@ const passwordi = document.getElementById('password');
 const emRegex = /^[A-Za-z0-9]{2,}$/;
 const usernameRegex = /^[A-Za-z]{6,}$/;
 const emailRegex = /^[A-Za-z0-9]{2,10}[@][a-z]{1,10}\.[a-z]{2,3}$/;
-const passiRegex = /^([A-za-z0-9]{8,})$/;
+const passiRegex = /^([A-Za-z0-9\S]{8,20})$/;
 
 function signUp(){
     if(!emRegex.test(emri.value)||!emRegex.test(mbiemri.value)||!usernameRegex.test(username.value)||!emailRegex.test(emaili.value)||!passiRegex.test(passwordi.value)){
@@ -40,5 +40,4 @@ function signUp(){
     alert(`Hello dear ${emri.value} shortly u will be redirected to the home page`);
     window.location.replace('../index.html');
 }
-   
 };
